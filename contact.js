6980c8b80e9267ac90957608688c417c4cc7e6bc
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-03-06 14:53:49 -0700

    adding js

---
+++ contact.js
@@ -0,0 +1,25 @@
+$("#sendInfo").on("click", function(event) {
+    event.preventDefault()
+    console.log("click");
+    alert("Thank you!")
+})
+
+$("#about").on("click", function(event) {
+    event.preventDefault()
+    console.log("click");
+})
+
+$("#experience").on("click", function(event) {
+    event.preventDefault()
+    console.log("click");
+})
+
+$("#portfolio").on("click", function(event) {
+    event.preventDefault()
+    console.log("click");
+})
+
+$("#contact").on("click", function(event) {
+    event.preventDefault()
+    console.log("click");
+})
